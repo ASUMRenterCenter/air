4424918a0d113e01de65917911f327bd59212a7e
kat09tails <kathryn.reese@umontana.edu>
2020-02-06 15:37:49 -0700

    Added some survey functionality and added navbar to all pages

--- src/index.js
+++ src/index.js
@@ -128,7 +128,6 @@ class Index extends React.Component {
 											id={this.state.org_acc_id}
 										/>
 									}
-									Sidebar={<Sidebar database={base} />}
 								/>
 								{/* <LoginPage {...props} database={base} /> */}
 							</div>
@@ -138,7 +137,7 @@ class Index extends React.Component {
 						path="/ButtonResults/:parent_name/:parent_id"
 						component={props => (
 							<div className="indexdivinner">
-								<ButtonResults {...props} database={base} />
+								<Navbar database={base} children={<ButtonResults {...props} database={base} />} />
 								<ExitButton />
 							</div>
 						)}
@@ -147,19 +146,19 @@ class Index extends React.Component {
 						path="/Survey"
 						component={() => (
 							<div className="indexdivinner">
-								<Survey database={base}/>
+								<Navbar database={base} children={<Survey database={base}/>} />
 								<ExitButton />
 							</div>
 						)}
 					/>
-					<Route
+					{/* <Route
 						path="/SurveyResults"
 						component={() => (
 							<div className="indexdivinner">
 								<SurveyResults />
 							</div>
 						)}
-					/>
+					/> */}
 					<Route
 						path="/CategoryResults/:taxonomy_name"
 						component={props => (
@@ -193,7 +192,7 @@ class Index extends React.Component {
 						path="/AgencyInfoPage/:agency_id"
 						component={(props) => (
 							<div className="indexdivinner">
-								<AgencyInfoPage {...props}/>
+								<Navbar database={base} children={<AgencyInfoPage {...props}/>} />
 							</div>
 						)}
 					/>
