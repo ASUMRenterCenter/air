109055659b07d0e282343df66a6893e3466d9999
kat09tails <kathryn.reese@umontana.edu>
2020-03-04 08:41:24 -0700

    bulletin board functionality

--- src/Javascript/bulletinboard.js
+++ src/Javascript/bulletinboard.js
@@ -8,16 +8,50 @@ export default class buttonresults extends React.Component{
 		super(props);
 		this.state = {
             events: [],
+            html: [],
             events_did_update: false,
 		}
     }
     componentDidMount (){
         this.props.database('bulletin_board').select({
         }).eachPage((events, fetchNextPage) => {
-            console.log(events)
-            this.setState(previousState => ({
-                events: [...this.state.events, ...events]            
-            }));
+            // this.setState(previousState => ({
+            //     events: [...this.state.events, ...events]            
+            // }));
+            events.map((event, index) => (
+                console.log(event.fields['services']),
+                this.props.database('services').find(event.fields['services'], (err, service) => {
+                    var addresses = []
+                    var phones = []
+                    var organizations = []
+                    console.log(service)
+                        if(service !== undefined){
+                        this.props.database('address').find(service.fields['address'], (err, address) => {
+                            console.log("Address")
+                            console.log(address)
+                            addresses.push(address.fields['address_1'])//Object.assign({}, address))
+                            addresses.push(address.fields['city'])
+                            addresses.push(address.fields['State'])
+                            addresses.push(address.fields['Zip Code'])
+                        })
+                        this.props.database('phones').find(service.fields['phones'], (err, phone) => {
+                            phones.push(phone.fields['number'])//Object.assign({}, phone))
+                        })
+                        this.props.database('organizations').find(service.fields['Organization'], (err, organization) => {
+                            organizations.push(organization.fields['name'].slice(0))//Object.assign({}, organization))
+                        })
+                        this.setState({
+                            html: 
+                                <div className='col'>
+                                    <h2>
+                                        {organizations[0]}
+                                    </h2>
+                                </div>
+                        })
+                    }
+                    
+                })
+            ));
         });
     }
 
@@ -26,6 +60,9 @@ export default class buttonresults extends React.Component{
     }
 
     render(){
-        return (<h1>This is a test</h1>);
+        if(this.state.events_did_update){
+            return <div>{this.state.html}</div>;
+        }
+        else return null;
     }
 };
