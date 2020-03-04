fcf01ac42f446ebefe361589923fe14819b3316f
Jesse <jesse.laflesch@umontana.edu>
2020-03-03 23:11:58 -0700

    huge changes

--- src/Javascript/history.js
+++
@@ -1,5 +0,0 @@
-/*Required for the homepage to render a certain button result after click. Needs to be external the the file it is used in or it won't work.*/
-
-import { createBrowserHistory } from 'history';
-
-export default createBrowserHistory({forceRefresh:true});
