202849f7ed03d6c9ee2bcecf2b2c961ec1e964da
Jesse <jesse.laflesch@umontana.edu>
2020-03-04 07:31:10 -0700

    minor fixes

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(pino);
 app.use(cors());
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/")));
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+	res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
 app.post("/login", function(req, res) {
