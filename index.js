077f0c4ae907bf5977e5f27b73198daccca6e897
Jesse <jesse.laflesch@umontana.edu>
2020-03-03 23:15:12 -0700

    fixes

--- index.js
+++ index.js
@@ -18,32 +18,12 @@ app.post("/login", function(req, res) {
 	res.send("got it");
 });
 
-/* // Serve our api route /cow that returns a custom talking text cow
-app.get("/api/cow/:say", cors(), async (req, res, next) => {
-	try {
-		const text = req.params.say;
-		const moo = cowsay.say({ text });
-		res.json({ moo });
-	} catch (err) {
-		next(err);
-	}
-});
-// Serve our base route that returns a Hello World cow
-app.get("/api/cow/", cors(), async (req, res, next) => {
-	try {
-		const moo = cowsay.say({ text: "Hello World!" });
-		res.json({ moo });
-	} catch (err) {
-		next(err);
-	}
-});
- */
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/public")));
 
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+	res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
 app.listen(3001, () =>
